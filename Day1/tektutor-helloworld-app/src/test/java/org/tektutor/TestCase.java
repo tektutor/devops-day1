81516c3ce8f63ad58ce26a8fe19255ab2893e36f
Train User <train@amdocs.com>
2017-03-01 15:05:53 +0530

    Added archetype demo project

---
+++ Day1/tektutor-helloworld-app/src/test/java/org/tektutor/TestCase.java
@@ -0,0 +1,13 @@
+package org.tektutor;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestCase
+{
+    @Test
+    public void testCase1()
+    {
+        assertTrue( 100 == 100 );
+    }
+}
